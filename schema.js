b176c4c44bcdbc6828772e2cfe7655ba235bb537
Aloksh42 <alokbits02@gmail.com>
2023-11-08 19:52:43 +0530

    ts-js

--- schema.js
+++ schema.js
@@ -4,22 +4,19 @@ export const typeDefs = `#graphql
     title: String!
     platform: [String!]!
   }
-
   type Review {
     id: ID!
-    rating: INT!
+    rating: Int!
     content: String!
   }
-
   type Author {
     id: ID!
     name: String!
     verified: Boolean!
   }
-
   type Query {
-    reviews: [Review]
     games: [Game]
+    reviews: [Review]
     authors: [Author]
   }
 `;
